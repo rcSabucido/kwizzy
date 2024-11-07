bb6ab46a633df5bdad592568ae8e3fe5a04c5b45
gabcabico <cabico.rudy@gmail.com>
2024-11-08 02:31:47 +0800

    Added clouds to PJV

--- backend/go.mod
+++ backend/go.mod
@@ -2,11 +2,13 @@ module quiz.com/quiz
 
 go 1.22.5
 
-require github.com/gofiber/fiber/v2 v2.52.5
+require (
+	github.com/gofiber/contrib/websocket v1.3.2
+	github.com/gofiber/fiber/v2 v2.52.5
+)
 
 require (
 	github.com/fasthttp/websocket v1.5.10 // indirect
-	github.com/gofiber/contrib/websocket v1.3.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/savsgio/gotils v0.0.0-20240704082632-aef3928b8a38 // indirect
 	golang.org/x/net v0.30.0 // indirect
@@ -15,7 +17,7 @@ require (
 require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/google/uuid v1.6.0
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- frontend/src/views/player/PlayerJoinView.svelte
+++ frontend/src/views/player/PlayerJoinView.svelte
@@ -7,7 +7,7 @@
 
     let code: string = "";
     let name: string = "";
-    export let game: PlayerGame
+    export let game: PlayerGame;
     
     function join(){
         dispatch("join");
@@ -19,10 +19,46 @@
     .custom-font {
         font-family: 'Wedges', sans-serif;
     }
+    .bg-blue-500 {
+        background-color: #3b82f6;
+        min-height: 100vh;
+        width: 100%;
+        position: relative;
+    }
+    .cloud {
+        background-image: url('/assets/cloud.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 100px; /* Adjust the size as needed */
+        height: 1000px;
+        position: absolute;
+    }
+    .cloud1 {
+        top: 20%;
+        left: 10%;
+    }
+    .cloud2 {
+        top: 30%;
+        right: 15%;
+    }
+    .cloud3 {
+        top: 50%;
+        left: 5%;
+    }
+    .cloud4 {
+        top: 60%;
+        right: 10%;
+    }
 </style>
 
+<div class="bg-blue-500 flex items-center justify-center">
+    <!-- Cloud elements -->
+    <div class="cloud cloud1"></div>
+    <div class="cloud cloud2"></div>
+    <div class="cloud cloud3"></div>
+    <div class="cloud cloud4"></div>
 
-<div class="bg-blue-500 Times min-h-screen w-full flex items-center justify-center">
+    <!-- Main content -->
     <div>
         <h2 class="text-white font-bold text-8xl text-center custom-font"> KWIZZY </h2>
         <div class="flex flex-col gap-2 mt-10 items-center">
@@ -31,5 +67,4 @@
             <button on:click={join} class="bg-amber-300 hover:bg-amber-400 text-black font-bold mt-2 rounded px-2 py-2"> Join Game </button>
         </div>
     </div>
-
 </div>
