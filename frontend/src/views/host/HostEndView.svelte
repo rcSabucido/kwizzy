3d53ef7a2e439c9194f9e5383ebe163a2e1d6c04
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-11-09 04:28:47 +0800

    Add styling to game ended view

--- frontend/src/views/host/HostEndView.svelte
+++ frontend/src/views/host/HostEndView.svelte
@@ -3,9 +3,10 @@
     import { leaderboard } from "../../service/host/host";
 </script>
 
-<div class="flex justify-center bg-purple-500 min-h-screen w-full">
+<div class="flex justify-center min-h-screen w-full"
+     style="background-color: #2a4674;">
     <div class="mt-32">
-        <h2 class="text-center text-white text-5xl font-bold">Game ended!</h2>
+        <h2 class="text-center text-yellow-400 text-5xl font-bold">Game ended!</h2>
         <div class="flex flex-wrap gap-2 mt-10">
             <Leaderboard finish={true} leaderboard={$leaderboard} />
         </div>
